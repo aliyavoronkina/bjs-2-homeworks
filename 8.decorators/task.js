da74cf8505d86c46b6e5680b5220bdf6e1ef2e9b
Aliya Voronkina <atleugabilova@mail.ru>
2025-12-13 01:01:42 +0500

    Fix

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,57 @@
-//Задача № 1
+// Задача № 1 - Усовершенствованный кеширующий декоратор
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+
+    const objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+
+    const result = func(...args);
+
+    cache.push({
+      hash: hash,
+      value: result
+    });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
 }
 
-//Задача № 2
+// Задача № 2 - Декоратор устранения дребезга
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (!timeoutId) {
+      func(...args);
+      wrapper.count++;
+    } else {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
